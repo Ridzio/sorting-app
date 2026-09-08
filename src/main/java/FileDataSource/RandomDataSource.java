bdec2e968158793bd18b4c80852dbfd0906b3bc0
Kelur's <yaroslav_ksk@mail.ru>
2026-09-08 16:29:48 +0500

    RandomDataSource is Fine

--- src/main/java/FileDataSource/RandomDataSource.java
+++ src/main/java/FileDataSource/RandomDataSource.java
@@ -1,4 +1,43 @@
 package FileDataSource;
 
-public class RandomDataSource {
+import com.team.sortingapp.core.ArrayCustomCollection;
+import com.team.sortingapp.core.CustomCollection;
+import com.team.sortingapp.core.DataSource;
+import com.team.sortingapp.core.Sortable;
+import com.team.sortingapp.core.Student;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class RandomDataSource<T extends Sortable> implements DataSource<T> {
+
+    private final Random random = new Random();
+
+    @Override
+    public CustomCollection<T> load(int length) {
+        if (length <= 0) {
+            return new ArrayCustomCollection<>(0);
+        }
+
+        List<T> tempList = Stream.generate(this::Generate)
+                .limit(length)
+                .map(student -> (T) student)
+                .toList();
+
+        return tempList.stream()
+                .collect(ArrayCustomCollection.toCustomCollection(tempList.size()));
+    }
+
+    private Student Generate() {
+        int groupNumber = random.nextInt(99999) + 1;
+        double averageGrade = random.nextDouble() * 5.0;
+        String recordBookNumber = String.valueOf(random.nextInt(99999) + 1);
+
+        return Student.builder()
+                .groupNumber(groupNumber)
+                .averageGrade(averageGrade)
+                .recordBookNumber(recordBookNumber)
+                .build();
+    }
 }
